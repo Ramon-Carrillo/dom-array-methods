d784864dee491c0f10e3f309803cd413ad94ced9
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-04-28 14:03:32 -0500

    finished the project

--- script.js
+++ script.js
@@ -32,6 +32,32 @@ const doubleMoney = () => {
   updateDOM()
 }
 
+//* Sort users by their money
+const sortByRichest = () => {
+  data = data.sort((a, b) => b.money - a.money)
+
+  updateDOM()
+}
+
+//* Filter only millionaires
+const filteredMillionaires = () => {
+  data = data.filter((user) => {
+    return user.money >= 1000000
+  })
+  updateDOM()
+}
+
+//* Calculate total value
+const totalWealth = () => {
+  const wealth = data.reduce((acc, user) => acc + user.money, 0)
+
+  const wealthEl = document.createElement('div')
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth,
+  )}</strong></h3>`
+  main.appendChild(wealthEl)
+}
+
 //* Add new obj to data arr
 const addData = (obj) => {
   data.push(obj)
@@ -65,6 +91,9 @@ const formatMoney = (amount) => {
 //* Event Listeners
 addUser.addEventListener('click', getRandomUser)
 double.addEventListener('click', doubleMoney)
+sort.addEventListener('click', sortByRichest)
+showMillionaires.addEventListener('click', filteredMillionaires)
+calculateWealth.addEventListener('click', totalWealth)
 
 getRandomUser()
 getRandomUser()
